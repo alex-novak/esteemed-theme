c34e872aca4a6a75ded8ca3c3baecb813b15b22c
Albert Volkman <albert.volkman@gmail.com>
2020-09-08 16:32:52 -0400

    Simplify menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -99,6 +99,22 @@ document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
   });
 });
 
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+  }
+  var $subMenu = $(this).next('.dropdown-menu');
+  $subMenu.toggleClass('show');
+
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass('show');
+  });
+
+
+  return false;
+});
+
 // Open headers menu on contractors by role page.
 $('.js-contractorsMenuBtn').on('click', function() {
   $(this).toggleClass('is-open');
