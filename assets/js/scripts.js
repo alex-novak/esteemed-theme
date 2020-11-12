376cafc9d01c547890bff3688ee50f0bd1937bf4
Alex Novak <alexn@celebratedrupal.com>
2020-11-12 22:00:20 +0200

    [cwp-40] Update partners page for desktop

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -206,3 +206,25 @@ $('.hamburger-top').on('click', function() {
     $('#main-wrapper').css('margin-top', '100px');
   }
 });
+
+$('.js-data-1').animate({ num: 200 - 20 }, {
+  duration: 2000,
+  step: function (num){
+  this.innerHTML = (num + 20).toFixed(0) + '+'
+}
+});
+
+$('.js-data-2').animate({ num: 6000 - 200 }, {
+  duration: 2000,
+  step: function (num){
+  this.innerHTML = (num + 200).toFixed(0) + '+'
+}
+});
+
+
+$('.js-data-3').animate({ num: 300 - 20 }, {
+  duration: 2000,
+  step: function (num){
+  this.innerHTML = (num + 20).toFixed(0)
+}
+});
